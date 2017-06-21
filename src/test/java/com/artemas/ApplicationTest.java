8f8d56cc13b804108786992668ed968bfeb37fa9
amuzanenhamo <artemasm@miless33.co.uk>
2017-06-21 14:39:24 +0100

    Added the Spring Boot Application Template

---
+++ src/test/java/com/artemas/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.artemas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by artemasm on 21/06/2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
